5c1a59a76a4770d840153047196cc399fe0d1fa5
giacomo12345 <84024694+giacomo12345@users.noreply.github.com>
2021-05-11 16:36:37 +0200

    Add files via upload

--- OpenImage/main.cpp
+++ OpenImage/main.cpp
@@ -1,19 +1,37 @@
+/* COMPUTER VISION - LAB 6 - AMBROSIN GIOELE - MULTINEDDU GIACOMO*/
+
 #include <opencv2/highgui.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/calib3d.hpp>
+#include <opencv2/core/utils/filesystem.hpp>
+#include <opencv2/core/utility.hpp>
+#include "opencv2/imgcodecs.hpp"
+#include <opencv2/core/base.hpp>
 #include <iostream>
+
 
 using namespace cv;
 using namespace std;
 
-int main(int argc, char **argv)
-{
-	cv::Mat img = cv::imread("immagine.png");
-	cv::namedWindow("Example1");
-	cv::imshow("Example1", img);
-	cv::waitKey(0);
+//-----------------------------------------------------functions
 
-	return 0;
-}
+//-----------------------------------------------------variables
 
 
 
 
+//------------------------------------------------------------------------------------
+//                               MAIN FUNCTION
+//------------------------------------------------------------------------------------
+int main(int argc, char* argv[]) {
+
+	if (argc < 2) {
+		std::cout << "Error - an image is needed" << std::endl;
+		return 0;
+	}
+	Mat img = imread(argv[1], IMREAD_COLOR);
+	resize(img, img, Size(img.cols / 2, img.rows / 2));
+	imshow("test window", img);
+
+	cv::VideoCaptu
