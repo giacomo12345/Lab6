43f0a908cea5b5899af88a42449b6e963fb2a109
gioeleambrosin <84024533+gioeleambrosin@users.noreply.github.com>
2021-05-11 16:57:38 +0200

    Add files via upload

--- OpenImage/main.cpp
+++ OpenImage/main.cpp
@@ -26,12 +26,38 @@ using namespace std;
 //------------------------------------------------------------------------------------
 int main(int argc, char* argv[]) {
 
+	/* upload images to track */
 	if (argc < 2) {
 		std::cout << "Error - an image is needed" << std::endl;
 		return 0;
 	}
+
 	Mat img = imread(argv[1], IMREAD_COLOR);
 	resize(img, img, Size(img.cols / 2, img.rows / 2));
 	imshow("test window", img);
+	
+	waitKey(1000);
 
-	cv::VideoCaptu
+	std::vector<cv::Mat> src;
+	cv::VideoCapture cap("video.mov");
+		if (cap.isOpened()) // check if we succeeded
+		{
+			for (;;)
+			{
+				cv::Mat frame;
+				cap >> frame;
+				src.push_back(frame);
+				
+			}
+		}
+	cout << src.size() << std::endl;
+	
+	waitKey(0);
+	return 0;
+}
+
+//------------------------------------------------------------------------------------
+//                                FUNCTIONS
+//------------------------------------------------------------------------------------
+
+
